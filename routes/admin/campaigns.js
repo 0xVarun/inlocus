c4e9549acf50066de19db0eb3cebb736e13773cb
Varun Tewari <vtewari26@gmail.com>
2019-02-11 11:28:45 +0530

    add campaigns boilerplate

---
+++ routes/admin/campaigns.js
@@ -0,0 +1,8 @@
+const express 			= require('express');
+const router			= express.Router();
+const User				= require('../../utils/User');
+const authMiddleware	= require('../../middleware/auth');
+
+
+
+module.exports = router;
--- routes/admin/routes.js
+++ routes/admin/routes.js
@@ -3,11 +3,13 @@ const router	= express.Router();
 
 const login		= require('./login');
 const register	= require('./register');
-const admin	= require('./admin');
+const admin		= require('./admin');
+const campaigns	= require('./campaigns');
 
 router.use('/login', login);
 router.use('/register', register);
 router.use('/home', admin);
+router.use('/campaigns', campaigns);
 router.use('/', (req, res) => { res.redirect('/admin/login'); })
 
 module.exports = router;
