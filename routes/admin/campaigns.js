c7a40888dceb0813024c48c44980932c89699c94
Varun Tewari <vtewari26@gmail.com>
2019-02-13 15:58:19 +0530

    updated campagin end date to always be after start date

--- routes/admin/campaigns.js
+++ routes/admin/campaigns.js
@@ -3,7 +3,6 @@ const router			= express.Router();
 const User				= require('../../utils/User');
 const authMiddleware	= require('../../middleware/auth');
 const appMiddleware		= require('../../middleware/active_application');
-const moment			= require('moment');
 
 router.get('/new', authMiddleware, (req, res) => {
 	res.render('admin/campaign', { title: 'New Campaign', layout: 'home' });
@@ -18,7 +17,13 @@ router.post('/create', authMiddleware, (req, res) => {
 	let enddate = req.body.enddate;
 	let starttime = req.body.starttime + ":00";
 	let endtime = req.body.endtime + ":00";
-	console.log(`${startdate}\t${enddate}\t${starttime}\t${endtime}`);
+
+	let fStart = new Date(`${startdate}, ${starttime} +0530`);
+	let fEnd = new Date(`${enddate} ${endtime} +0530`);
+
+	console.log('start '+  fStart);
+	console.log('end ' + fEnd);
+
 	res.redirect('/admin/campaigns/new');
 })
 
