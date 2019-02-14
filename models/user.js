ee3f038cb9110fd6535e58764fa48351870de10b
Varun Tewari <vtewari26@gmail.com>
2019-02-14 12:07:46 +0530

    add charts and profile page

--- models/user.js
+++ models/user.js
@@ -6,10 +6,23 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     active: DataTypes.BOOLEAN,
-    superadmin: DataTypes.BOOLEAN
+    superadmin: DataTypes.BOOLEAN,
+    appId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'application',
+        key: 'id'
+      }
+    }
   }, {});
   user.associate = function(models) {
     // associations can be defined here
+    user.hasOne(models.application, {
+      foreignKey: {
+        name: 'id',
+        allowNull: false
+      }
+    })
   };
   return user;
 };
