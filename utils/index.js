164fdae0a13394812af24ee5d83a915a15aa4a58
Varun Tewari <vtewari26@gmail.com>
2019-04-17 12:29:05 +0530

    integrate all db utils into one file

---
+++ utils/index.js
@@ -0,0 +1,15 @@
+
+const utilExports = {
+    Application: require('./Application'),
+    BeaconMaster: require('./BeaconMaster'),
+    Campaign: require('./Campaign'),
+    Device: require('./Device'),
+    GeoFence: require('./GeoFence'),
+    GeoLoc: require('./GeoLoc'),
+    LocationMaster: require('./LocationMaster'),
+    SdkUser: require('./SdkUser'),
+    Sensor: require('./Sensor'),
+    User: require('./User')
+}
+
+module.exports = utilExports;
