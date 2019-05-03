e3d2e493167c650f73fb40bf98fa90fa492395ba
Varun Tewari <vtewari26@gmail.com>
2019-05-03 12:14:51 +0530

    added sensor type to route and update save method to save sensor type as well

--- routes/app/api/notify.js
+++ routes/app/api/notify.js
@@ -3,12 +3,13 @@ const router 		= express.Router();
 const utils 		= require('../../../utils');
 const apiMiddleware = require('../../../middleware/api').apiAuth;
 
-router.get('/:id', apiMiddleware, async (req, res) => {
+router.get('/:id/:type', apiMiddleware, async (req, res) => {
     let deviceId = res.locals.user['deviceId'];
     let appId   = res.locals.user['appId'];
     let campaignId = req.params.id;
+    let type = req.params.id;
     
-    await utils.Notify.clicked("CLICKED", deviceId, appId, campaignId);
+    await utils.Notify.clicked("CLICKED", deviceId, appId, campaignId, type);
 
     res.sendStatus(200);
 });
--- utils/Notify.js
+++ utils/Notify.js
@@ -1,22 +1,22 @@
 const db		= require('../db/postgres');
 const model		= require('../models');
 
-module.exports.clicked = async (status, deviceId, appId, campaignId) => {
+module.exports.clicked = async (status, deviceId, appId, campaignId, type) => {
 
-    let existing = await model.notify.findOne({ where: { deviceId: deviceId, campaignId: campaignId, applicationId: appId } });
+    let existing = await model.notify.findOne({ where: { deviceId: deviceId, campaignId: campaignId, applicationId: appId , type: type} });
 
     if(existing) {
         model.notify.findOne({
-            where: { deviceId: deviceId, campaignId: campaignId, applicationId: appId },
+            where: { deviceId: deviceId, campaignId: campaignId, applicationId: appId, type: type },
             order: [['createdAt', 'DESC']]
         }).then(notify => {
             notify.update({ status: status });
         })
     } else {
-        await model.notify.create({ status: status, deviceId: deviceId, applicationId: appId, campaignId: campaignId });
+        await model.notify.create({ status: status, deviceId: deviceId, applicationId: appId, campaignId: campaignId, type: type });
     }    
 }
 
 module.exports.sent = async (status, deviceId, appId, campaignId) => {
-    await model.notify.create({ status: status, deviceId: deviceId, applicationId: appId, campaignId: campaignId });
+    await model.notify.create({ status: status, deviceId: deviceId, applicationId: appId, campaignId: campaignId, type: type });
 }
