f70a9248c4c9d130312684947486fecbe0aaa7de
Varun Tewari <vtewari26@gmail.com>
2019-05-07 12:29:00 +0530

    enable beacon campaigns

--- tags.js
+++ tags.js
@@ -2,8 +2,7 @@
 // process.env.GOOGLE_PLACES_OUTPUT_FORMAT = 'json';
 
 const http = require('axios');
-url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyD0awMda5obf1OnLxaJ1gqghu2A19Fr7Bs&latlng=19.116230,72.909851&sensor=true"
-
+const models = require('./models');
 
 async function getAddress(api_key, lat, lng) {
 
@@ -13,5 +12,24 @@ async function getAddress(api_key, lat, lng) {
 	console.log(res.data.results[0]['formatted_address']);
 }
 
-getAddress('AIzaSyD0awMda5obf1OnLxaJ1gqghu2A19Fr7Bs', 19.116230, 72.909851);
+// getAddress('AIzaSyD0awMda5obf1OnLxaJ1gqghu2A19Fr7Bs', 19.116230, 72.909851);
 
+
+async function createLocations(api_key) {
+
+	let data = await models.location.findOne({ where: { address: null } });
+
+
+	let lat = data.latitude;
+	let lng = data.longitude;
+
+	let url = `https://maps.googleapis.com/maps/api/geocode/json?key=${api_key}&latlng=${lat},${lng}&sensor=true`;
+
+	let req = await http.get(url);
+	let address = req.data.results[0]['formatted_address']
+	let x = await data.update({ address:address });
+	console.log(JSON.stringify(x));
+
+}
+
+createLocations('AIzaSyD0awMda5obf1OnLxaJ1gqghu2A19Fr7Bs');
